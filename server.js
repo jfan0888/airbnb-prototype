9dc2b679d728e713ae160b0da36533c50d5fe67d
jfan0888 <bsrg5111@gmail.com>
2019-10-24 13:24:22 -0700

    added server script for the deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3001;
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log('Listening on Port', port));
