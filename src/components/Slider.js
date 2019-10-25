9594d4592fec9f7549487f0ef512ba6ab1624a87
jfan0888 <bsrg5111@gmail.com>
2019-10-25 03:46:11 -0700

    added custom slider

---
+++ src/components/Slider.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactSlider from 'react-slider';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripLinesVertical } from '@fortawesome/free-solid-svg-icons';
+
+const StyledSlider = styled(ReactSlider)`
+  width: 100%;
+  height: 3px;
+`;
+
+const StyledThumb = styled.div`
+  height: 25px;
+  line-height: 25px;
+  width: 25px;
+  text-align: center;
+  border: 1px solid #07878b;
+  background-color: #fff;
+  color: #07878b;
+  border-radius: 50%;
+  cursor: grab;
+  bottom: -12.5px;
+  font-size: 10px;
+`;
+
+const Thumb = (props, state) => (
+  <StyledThumb {...props}>
+    <FontAwesomeIcon icon={faGripLinesVertical} />
+  </StyledThumb>
+);
+
+const StyledTrack = styled.div`
+  top: 0;
+  bottom: 0;
+  background: ${props =>
+    props.index === 2 ? '#ddd' : props.index === 1 ? '#07878b' : '#ddd'};
+  border-radius: 999px;
+`;
+
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
+const Slider = props => (
+  <StyledSlider
+    defaultValue={[50, 75]}
+    renderTrack={Track}
+    renderThumb={Thumb}
+  />
+);
+
+export default Slider;
