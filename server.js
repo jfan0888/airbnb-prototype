1d210701ba4fe8232c43d6c2c6bae88af9f10973
jfan0888 <bsrg5111@gmail.com>
2019-10-24 18:29:33 -0700

    updated server path script

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const buildPath = path.join(__dirname, 'build');
 
-app.use('/', express.static(__dirname));
+app.use('/', express.static(buildPath));
 app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, 'index.html'));
+  response.sendFile(path.resolve(buildPath, 'index.html'));
 });
 
 app.listen(port, () => console.log('Listening on Port', port));
