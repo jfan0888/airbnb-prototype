9da9c68820a43530aadd52e207e5f238581e9239
jfan0888 <bsrg5111@gmail.com>
2019-10-24 18:26:27 -0700

    updated server scripts for the heroku deployment

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'build')));
+app.use('/', express.static(__dirname));
 app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+  response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port, () => console.log('Listening on Port', port));
