92255abbea271225060be302f200cecf62845f43
jfan0888 <bsrg5111@gmail.com>
2019-10-28 08:16:10 -0700

    refactored components importing

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import SidebarToggle from './SidebarToggle';
 import Slider from './Slider';
 import DateInput from './DateInput';
 import Sidebar from './Sidebar';
+import Dropdown from './Dropdown';
+import TabBar from './TabBar';
+import DataRange from './DataRange';
 
-export { Button, Loading, SidebarToggle, Slider, DateInput, Sidebar };
+export {
+  Button,
+  Loading,
+  SidebarToggle,
+  Slider,
+  DateInput,
+  Sidebar,
+  Dropdown,
+  TabBar,
+  DataRange,
+};
