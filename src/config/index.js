9c41b49e690398fc14aefb2829acf7f21a3e43e9
jfan0888 <bsrg5111@gmail.com>
2019-11-04 00:56:49 -0800

    added center geo coordinates

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 const mapboxToken =
   'pk.eyJ1IjoiaWFzaGlzaHNpbmdoIiwiYSI6ImNqbHQ2d2JpcTA1cnEzcG9idGl2aTN5MTMifQ.yGUYoyQ4IyAfzl3E2D0ecA';
+const centerPosition = [-99.9, 41.5];
 
-export { mapboxToken };
+export { mapboxToken, centerPosition };
