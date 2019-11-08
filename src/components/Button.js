451eb232e9abcb86220f8a0e0bbaf7982f803f80
jfan0888 <bsrg5111@gmail.com>
2019-11-08 06:08:48 -0800

    linting button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ title, clickHanlder, type }) => (
-    <button className={`btn ${type}`} onClick={clickHanlder}>{title}</button>
-)
+  <button className={`btn ${type}`} onClick={clickHanlder}>
+    {title}
+  </button>
+);
 
 Button.propTypes = {
-    title: PropTypes.string,
-    clickHanlder: PropTypes.func,
-    type: PropTypes.string,
-}
+  title: PropTypes.string,
+  clickHanlder: PropTypes.func,
+  type: PropTypes.string,
+};
 
 Button.defaultProps = {
-    title: 'Button',
-    type: '',
-    clickHanlder: () => { }
-}
+  title: 'Button',
+  type: '',
+  clickHanlder: () => {},
+};
 
 export default Button;
